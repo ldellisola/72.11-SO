e2d5af9e6bfd644c0ad25920ef7b2f4be1743da5
TamiP99 <tamypuig@gmail.com>
2020-03-30 18:39:50 -0300

    .

--- TP1/slave.c
+++ TP1/slave.c
@@ -67,7 +67,7 @@ int main(int argc,char ** argv){
         FILE * fp;
 
         char inst[MAX];
-        snprintf(inst,MAX,"minisat %s |grep -o -e 'Number of.*[0-9]\\+' -e 'CPU time.*' -e '.*SATISFIABLE' | grep -o -e '[0-9|.]*' -o -e '.*SATISFIABLE' | xargs | sed 's/ /\t/g'",file);
+        snprintf(inst,MAX,"minisat %s |grep -o -e 'Number of.*[0-9]\\+' -e 'CPU time.*' -e '.*SATISFIABLE' | grep -o -e '[0-9|.]*' -o -e '.*SATISFIABLE' | xargs",file);
   		fp = popen(inst, "r");
         if (fp == NULL){
             perror("Popen error:");
@@ -85,12 +85,12 @@ int main(int argc,char ** argv){
         }
         ans[ansSize]=0;
 
-        int v,c,t=0;
+        int v,c;
+        float t;
         char s[15];
-        sscanf(ans,"%d %d %d %s",&v,&c,&t,s);
-
+        sscanf(ans,"%d %d %f %s",&v,&c,&t,s);
         char info[MAX*2]; 
-        n = snprintf(info,MAX,"PID: %d. File: %s. Number of Variables: %d. Number of Clauses: %d. CPU TIME: %d. %s \n",getpid(),file,v,c,t,s);
+        n = snprintf(info,MAX,"PID: %d. File: %s. Number of Variables: %d. Number of Clauses: %d CPU TIME: %f %s \n",getpid(),file,v,c,t,s);
         // printf(info);
         write(STDOUT_FILENO,info,n);
 
