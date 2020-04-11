98d2d0032c4f7b2fca76bb5c43d299b352a30cc0
TamiP99 <tamypuig@gmail.com>
2020-04-11 17:37:56 -0300

    initial message

--- TP2/Userland/SampleCodeModule/sampleCodeModule.c
+++ TP2/Userland/SampleCodeModule/sampleCodeModule.c
@@ -7,7 +7,7 @@
 #include "../Include/Curses.h"
 #include <stdlib.h>
 #include "../Include/deviceInfo.h"
-void menu(){
+/*void menu(){
 	unsigned int init;
 	unsigned int aux;
 	unsigned int charHeight;
@@ -20,15 +20,16 @@ void menu(){
 	
 	int position=67;
 	int y=30;
-	printfColorAt(0xFF44FF,0x00,position,y,"TPE DE PC");
+	printfColorAt("TPE DE PC",0xFF44FF,0x00,position,y);
     setSize(init*2);
 	getBpp(&aux);
     y+=charHeight*aux+3;
-    printfColorAt(0xFFFFFF,0x00,position,y,"58025,Dellisola   58489,Torrusio   59820,PUIG");
+    printfColorAt("58025,Dellisola   58489,Torrusio   59820,PUIG",0xFFFFFF,0x00,position,y);
     y+=charHeight*aux+3;
-    printfColorAt(0xFFFFFF,0x00,position*4+30,y,"1) Terminal");
+    printfColorAt("1) Game",0xFFFFFF,0x00,position*2,y);
+    printfColorAt("2) Terminal",0xFFFFFF,0x00,position*4+30,y);
 	y+=charHeight+20;
-	printfColorAt(0xFFFFFF,0x00,position*3+20,y,"2) Exit");
+	printfColorAt("3) Exit",0xFFFFFF,0x00,position*3+20,y);
     setSize(init);
 
 }
@@ -44,18 +45,30 @@ int main() {
 		if(selector == -1){
 			clearConsole();
 			menu();
-			while(((selector=readKey())!='1') && (selector!='2'));
+			while(((selector=readKey())!='1') && (selector!='2') && (selector!='3'));
 			clearConsole();
-			if(selector == '1')
+			if(selector == '2')
 				printf("HI! Ask \"man\" for a command menu\n");
 		}
 
-		if(selector == '1'){
+		if(selector == '2'){
 
 			
 			switch(runTerminal()){
 				case 1: exit = 1; break;
+				case 2: selector = '1'; break;
 			}
+		}
+		else if(selector == '1'){
+			clearConsole();
+			switch(runGame()){
+				case 1: exit = 1; break;
+				default: selector = '2'; break;
+			}
+			clearConsole();
+			if(selector == '2')
+				printf("HI! Ask \"man\" for a command menu\n");
+
 		}
 		else{
 			exit = 1;
@@ -73,7 +86,23 @@ int main() {
 	
 	int position=70;
 	int y=50;
-	printfColorAt(0xFF44FF,0x00,position,y,"BYE!");
+	printfColorAt("BYE!",0xFF44FF,0x00,position,y);
+	return 0xDEA;
+}*/
+
+
+int main() {
+	clearConsole();
+	printf("HI! Ask \"man\" for a command menu\n");
+	runTerminal();
+	clearConsole();
+
+	unsigned int init=0;
+	getBpp(&init);
+	setSize(6*init);
+	
+	int position=70;
+	int y=50;
+	printfColorAt(0xFF44FF,0x00,position,y,"Bye!");
 	return 0xDEA;
 }
-
