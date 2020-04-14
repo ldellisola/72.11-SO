42a1d1a3b25054c8656120945682746b62a44a7c
Lucas Dell'Isola <ldellisola@itba.edu.ar>
2020-04-14 14:01:43 -0300

    Corregi la declaracion de srbk_handler

--- TP2/Kernel/include/sbrk.h
+++ TP2/Kernel/include/sbrk.h
@@ -2,7 +2,7 @@
 #define SBRK_H
 #include <stdint.h>
 
-void sbrk_handler(intptr_t increment, void ** buffer);
+void sbrk_handler(int increment, void ** buffer);
 int brk_handler( void * addr);
 
 #endif
