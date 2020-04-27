16583ddad4666c9b939dde1f0f47a6975e47fa59
Lucho <lucho@localhost.localdomain>
2020-04-27 18:31:38 -0400

    Added functionality of loop every 5 seconds, tested successfuly in foreground

--- TP2/Userland/SampleCodeModule/Process.c
+++ TP2/Userland/SampleCodeModule/Process.c
@@ -2,20 +2,40 @@
 #include "../Include/Syscalls.h"
 #include "../Include/Time.h"
 #include "include/Process.h"
+#include <stdbool.h>
 
-#define LOOP_TIME 5 //1seg
+#define LOOP_TIME 5 // N secs
 
 void loop()
 {
-    int t1 = GetSeconds();
-    int t2 = 0;
+    bool flag = true;
+    unsigned int startSeconds = GetSeconds();
+    unsigned int t1 = 0;
+    unsigned int t2 = t1;
+    int pid = getpid();
+    unsigned int elapsed = 0;
     do
     {
-        // if(++t2 %2000000 == 0)
-        // {
-        //     printf("HOLA\n");
-        // }
-        // if (t2 == -1 || t2 - t1 >= LOOP_TIME)
-         //   printf("Hey, im %d. See you in %d s", getpid(), LOOP_TIME);
+        if ((GetSeconds() - startSeconds) >= 59) { // because when more than 1 minute has elapsed timer explodes
+            t1 -= startSeconds;                    // keep the timer upon update
+            t2 -= startSeconds;
+            startSeconds = GetSeconds();
+        }
+        else
+        {
+            t2 = GetSeconds() - startSeconds;
+        }
+
+        if (((elapsed % LOOP_TIME) == 0 ) && flag) {
+            printf("Loop says: My PID is %d. BRB in %d secs\n", pid, LOOP_TIME);
+            flag = false;                           //avoid more than 1 call per second
+        }
+        
+        if (t2 != t1) {                             // != and not > because of the first if
+            flag = true;
+            t1 = t2;
+            // printf("Elapsed: %d\n", elapsed);
+            elapsed++;
+        }
     } while (1);
 }
