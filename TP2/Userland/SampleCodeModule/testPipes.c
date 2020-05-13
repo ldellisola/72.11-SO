f679e62620833ee08c219a9eee7a0a0d4ba3f76b
TamiP99 <tamypuig@gmail.com>
2020-05-13 01:09:45 -0300

    please

---
+++ TP2/Userland/SampleCodeModule/testPipes.c
@@ -0,0 +1,23 @@
+#include "include/Process.h"
+#include "../Include/Pipes.h"
+#include "../Include/Syscalls.h"
+#include "include/Exec.h"
+#include "../Include/String.h"
+
+
+void testPipes(){
+  int fdw=openPipe("hola",WRITE);
+  int fdr=openPipe("hola",READ);
+  int x=0;
+  char aux[100];
+  char buffer[1024];
+  while(x!=1000000000){
+    x++;
+    IntToString(aux,100,x);
+    writePipe(fdw,aux);
+    readPipe(fdr,buffer,1024);
+    printf("lei %s\n",buffer);
+  }
+  //pipe();
+}
+
