55adda5075ba9bc4a8140ed2061c3a17c9a8f99a
lboccardi <lboccardi@itba.edu.ar>
2020-05-20 19:11:41 -0400

    same

---
+++ TP2/Userland/SampleCodeModule/include/phylo.h
@@ -0,0 +1,23 @@
+// void * philosopher_process(void * num);
+
+enum State {ASLEEP = 0, EATING};
+
+void philosopher_process();
+
+void kill_philosopher();
+
+void create_philosopher();
+
+// char * philo_p_name (int phnum);
+
+// char * philo_sem_name (int phnum);
+
+void take_fork(int phnum);
+
+void put_fork(int phnum);
+
+void test(int phnum);
+
+int left (int phnum);
+
+int right (int phnum);
