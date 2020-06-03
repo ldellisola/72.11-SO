627a8b5ea033e558654678625766b0b730d313ed
TamiP99 <tamypuig@gmail.com>
2020-06-02 23:53:24 -0300

    fd mixed ok!

--- TP3/server.c
+++ TP3/server.c
@@ -12,6 +12,8 @@
 #define MAXBUFLEN 500
 
 void challenges();
+void mixedFds();
+
 
 int main(int argc, char *argv[]){
 int sockfd;
@@ -75,23 +77,59 @@ return 0;
 
 void challenges (){
 int challenge = 0;
-char * message[] = {"Para asegurarnos que haya entendido, escriba \"grupo 4 es el mejor\\n\"\n"};
-char * ans[] = {"grupo 4 es el mejor\n"};
+char * message[] = {
+  "Para asegurarnos que haya entendido, escriba \"grupo 4 es el mejor\\n\"\n",
+  "Mixed fds\n"
+};
+char * ans[] = {
+  "grupo 4 es el mejor",
+  "a_non_y_mous"
+};
 char buffer[MAXBUFLEN];
 while(1){
   printf("\n--- DESAFIO %d ---\n\n",challenge+1);
   printf("%s",*(message+challenge));
+  switch (challenge)
+  {
+  //va a cambiar al que corresponda el de 2FDS  
+  case 1:
+    mixedFds();
+    break;
+  default: break;
+  }
   int c=read(4,buffer,MAXBUFLEN);
   buffer[c-1]=0;
-  if(strcmp(*(ans+challenge),buffer)){
-    printf("Bien!\n");
+  printf("\nbuffer : %s and ans: %s\n",buffer,*(ans+challenge));
+  if(strcmp(*(ans+challenge),buffer) == 0){
+    printf("tin tin tin CORRECTO!\n");
     challenge++;
   }
   else
   {
-    printf("Respuesta incorrecta: %s",buffer);
+    printf("Respuesta incorrecta: %s\n\n",buffer);
   }
   
 } 
 
+}
+
+void mixedFds(){
+  char * ans = "a_non_y_mous";
+  char * trash = "asda213123s[*]*[$dasd23436*12}+{+{dasd234asd43as}-}31}+asd43as}{+{+-**-+dsfd\\¿'¿ASDA213123S[*]*[$Dasd234asd43as}{+{+-**-+dsfd\\¿'¿''dasd5636*12}+{+{-}31}+{3123sdfsadf23''daasdasdasdasdfgsdsd5636*12}+{+{daasd43as}{+{+-**-+dsfd\\¿'¿''dasd5636*12}+{sd234asd43as}-}31}+{3123s5461685as65das5df4ur890'34rhebwidsklmkñaiofpsu8g9hibnjreklm4,3lwqe'pdsñ{.dfsadf23a";
+  int x = (strlen(trash)/strlen(ans));
+  int tr,rsp,i;
+  tr = rsp = i = 0;
+  printf("\n\n");
+  while(rsp!=strlen(ans)){
+    if( i%2 == 1){
+      write(1,(ans+rsp++),1);
+    }
+    else {
+      write(2,trash+tr,x);
+      tr+=x;
+    }
+    i++;
+  }
+    write(2,trash,x/2);
+  printf("\n");
 }
